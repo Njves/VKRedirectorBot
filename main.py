06144e4421e21e1c8e1ce1faac330badda6e3d11
Njves <mrpostik@gmail.com>
2023-07-17 22:29:35 +0700

    Добавил парсинг поста

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.responses import PlainTextResponse
+from config import TOKEN_VK, TOKEN_CONF_VK
+from datetime import datetime
+
 
 class Confirmation(BaseModel):
     type: str
     group_id: int
 
 
+class Photo(BaseModel):
+    id: str
+    type: str
+    small_photo: str
+    big_photo: str
+
+
+class Video(BaseModel):
+    id: str
+    title: str
+    description: str
+    duration: int
+    player: str
+
+
+class Attachment:
+    type: str
+    photo: Photo
+
+
+class Post(BaseModel):
+    id: str
+    date: datetime
+    text: str
+    attachments: list[Attachment]
+
+
+class Event(BaseModel):
+    type: str
+    event_id: str
+    v: float
+    group_id: str
+    object: Post
+
+
 app = FastAPI()
 
 
-@app.post("/conf", response_class=PlainTextResponse)
-async def create_item(confirmation: Confirmation):
-    return '0fc10f4c'
-
-
+@app.post("/")
+async def create_item(query: str):
+    event: Event = Event.parse_raw(query)
+    match event.type:
+        case 'confirmation':
+            return TOKEN_CONF_VK
+        case 'wall_post_new':
+            print(event.object)
--- postman.py
+++ postman.py
@@ -1,4 +1,4 @@
 import requests
 
 req = requests.post('https://vkredirectorbot.onrender.com/conf', json={'type': 'confirmation', 'group_id': 216438878})
-print(req.json())
+print(req.text)
